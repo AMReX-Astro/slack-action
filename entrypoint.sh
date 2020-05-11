34e8ef5e5b96c57a9f8ee7d6f07b144022ce9a62
Alice Harpole <aliceharpole@gmail.com>
2020-05-11 16:49:24 -0400

    add more links

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ fi
 
 echo '{"attachments": [{"mrkdwn_in": ["text","title"], "color": "#000000", "author_name": "'"$GITHUB_ACTOR"'","author_link": "https://github.com/'"$GITHUB_ACTOR"'","author_icon": "'"$INPUT_AUTHOR_AVATAR"'", "text": "*<'"$INPUT_PR_LINK/$INPUT_COMMIT_SHA"'|1 new commit> pushed to `'"$GITHUB_HEAD_REF"'`*\n<'"$INPUT_PR_LINK/$INPUT_COMMIT_SHA"'|`'"${INPUT_COMMIT_SHA::8}"'`> - '"$INPUT_MESSAGE"'"} ]}'
 
-curl -X POST -H "Content-type: application/json" --data '{"attachments": [{"mrkdwn_in": ["text","title"], "color": "#000000", "author_name": "'"$GITHUB_ACTOR"'","author_link": "https://github.com/'"$GITHUB_ACTOR"'","author_icon": "'"$INPUT_AUTHOR_AVATAR"'", "text": "*<'"$INPUT_PR_LINK/$INPUT_COMMIT_SHA"'|1 new commit> pushed to `'"$GITHUB_HEAD_REF"'`*\n<'"$INPUT_PR_LINK/$INPUT_COMMIT_SHA"'|`'"${INPUT_COMMIT_SHA::8}"'`> - '"$INPUT_MESSAGE"'"} ]}' $INPUT_SLACK_WEBHOOK
+curl -X POST -H "Content-type: application/json" --data '{"attachments": [{"mrkdwn_in": ["text","title"], "color": "#000000", "author_name": "'"$GITHUB_ACTOR"'","author_link": "https://github.com/'"$GITHUB_ACTOR"'","author_icon": "'"$INPUT_AUTHOR_AVATAR"'", "text": "*<'"$INPUT_PR_LINK/compare/${INPUT_BEFORE_SHA::8}..${INPUT_COMMIT_SHA::8}"'|1 new commit> pushed to <'"$INPUT_PR_LINK/tree/$GITHUB_HEAD_REF"'|`'"$GITHUB_HEAD_REF"'`>*\n<'"$INPUT_PR_LINK/commit/$INPUT_COMMIT_SHA"'|`'"${INPUT_COMMIT_SHA::8}"'`> - '"$INPUT_MESSAGE"'"} ]}' $INPUT_SLACK_WEBHOOK
 
 
 # {
