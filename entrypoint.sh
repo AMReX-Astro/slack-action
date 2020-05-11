f154d28bd760e1073067a1861448327b8d7aa3f1
Alice Harpole <aliceharpole@gmail.com>
2020-05-11 15:11:28 -0400

    remove extra quotation mark

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,9 @@ fi
 
 # curl -X POST -H "Content-type: application/json" --data '{"text":"'"$INPUT_MESSAGE"'"}' $INPUT_SLACK_WEBHOOK
 
-curl -X POST -H "Content-type: application/json" --data '{"attachments": [{"mrkdwn_in": ["text"],"author_name": "'"$GITHUB_ACTOR"'","author_link": "https://github.com/'"$GITHUB_ACTOR"'","title": "1 new commit pushed to '"$GITHUB_HEAD_REF"'","title_link": "'"$INPUT_PR_LINK"'","text": "`'"${GITHUB_SHA::8}"'"` - '"$INPUT_MESSAGE"'"} ]}' $INPUT_SLACK_WEBHOOK
+echo '{"attachments": [{"mrkdwn_in": ["text"],"author_name": "'"$GITHUB_ACTOR"'","author_link": "https://github.com/'"$GITHUB_ACTOR"'","title": "1 new commit pushed to '"$GITHUB_HEAD_REF"'","title_link": "'"$INPUT_PR_LINK"'","text": "`'"${GITHUB_SHA::8}"'` - '"$INPUT_MESSAGE"'"} ]}'
+
+curl -X POST -H "Content-type: application/json" --data '{"attachments": [{"mrkdwn_in": ["text"],"author_name": "'"$GITHUB_ACTOR"'","author_link": "https://github.com/'"$GITHUB_ACTOR"'","title": "1 new commit pushed to '"$GITHUB_HEAD_REF"'","title_link": "'"$INPUT_PR_LINK"'","text": "`'"${GITHUB_SHA::8}"'` - '"$INPUT_MESSAGE"'"} ]}' $INPUT_SLACK_WEBHOOK
 
 
 # {
