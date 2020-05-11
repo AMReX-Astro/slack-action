4734cf3529d5e508f3616bbddadccdfef29e939a
Alice Harpole <aliceharpole@gmail.com>
2020-05-11 12:40:47 -0400

    fix payload

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,8 @@ if test -z "$INPUT_SLACK_WEBHOOK"; then
   exit 1
 fi
 
-curl -X POST -H "Content-type: application/json" --data '{"text":$INPUT_MESSAGE}' $INPUT_SLACK_WEBHOOK
+SLACK_PAYLOAD='{"text":"$INPUT_MESSAGE"}'
+
+echo "PAYLOAD = $SLACK_PAYLOAD"
+
+curl -X POST -H "Content-type: application/json" --data '{"text":"$INPUT_MESSAGE"}' $INPUT_SLACK_WEBHOOK
