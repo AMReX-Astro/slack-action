9fc35980c86be086e4b39e60c7dd5605ccc984a8
Alice Harpole <aliceharpole@gmail.com>
2020-05-11 16:29:56 -0400

    fix title link

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,9 @@ fi
 
 # curl -X POST -H "Content-type: application/json" --data '{"text":"'"$INPUT_MESSAGE"'"}' $INPUT_SLACK_WEBHOOK
 
-echo '{"attachments": [{"mrkdwn_in": ["text"],"author_name": "'"$GITHUB_ACTOR"'","author_link": "https://github.com/'"$GITHUB_ACTOR"'","title": "1 new commit pushed to '"$GITHUB_HEAD_REF"'","title_link": "'"$INPUT_PR_LINK"'","text": "`'"${GITHUB_SHA::8}"'` - '"$INPUT_MESSAGE"'"} ]}'
+echo '{"attachments": [{"mrkdwn_in": ["text","title"], "color": "#000000", "author_name": "'"$GITHUB_ACTOR"'","author_link": "https://github.com/'"$GITHUB_ACTOR"'","author_icon": "'"$INPUT_AUTHOR_AVATAR"'", "text": "*<'"$INPUT_PR_LINK/$INPUT_COMMIT_SHA"'|1 new commit> pushed to `'"$GITHUB_HEAD_REF"'`*\n<'"$INPUT_PR_LINK/$INPUT_COMMIT_SHA"'|`'"${INPUT_COMMIT_SHA::8}"'`> - '"$INPUT_MESSAGE"'"} ]}'
 
-curl -X POST -H "Content-type: application/json" --data '{"attachments": [{"mrkdwn_in": ["text","title"], "color": "#000000", "author_name": "'"$GITHUB_ACTOR"'","author_link": "https://github.com/'"$GITHUB_ACTOR"'","author_icon": "'"$INPUT_AUTHOR_AVATAR"'", "title": "[1 new commit]('"$INPUT_PR_LINK/$INPUT_COMMIT_SHA"') pushed to `'"$GITHUB_HEAD_REF"'`","text": "`'"${INPUT_COMMIT_SHA::8}"'` - '"$INPUT_MESSAGE"'"} ]}' $INPUT_SLACK_WEBHOOK
+curl -X POST -H "Content-type: application/json" --data '{"attachments": [{"mrkdwn_in": ["text","title"], "color": "#000000", "author_name": "'"$GITHUB_ACTOR"'","author_link": "https://github.com/'"$GITHUB_ACTOR"'","author_icon": "'"$INPUT_AUTHOR_AVATAR"'", "text": "*<'"$INPUT_PR_LINK/$INPUT_COMMIT_SHA"'|1 new commit> pushed to `'"$GITHUB_HEAD_REF"'`*\n<'"$INPUT_PR_LINK/$INPUT_COMMIT_SHA"'|`'"${INPUT_COMMIT_SHA::8}"'`> - '"$INPUT_MESSAGE"'"} ]}' $INPUT_SLACK_WEBHOOK
 
 
 # {
