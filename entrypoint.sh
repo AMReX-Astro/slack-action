fdef17a9700812abe7a6d1183e021340c3136c4c
Alice Harpole <aliceharpole@gmail.com>
2020-05-11 14:53:43 -0400

    try doing fancy formatting

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,20 @@ if test -z "$INPUT_SLACK_WEBHOOK"; then
   exit 1
 fi
 
-SLACK_PAYLOAD='{"text":"'$INPUT_MESSAGE'"}'
+# curl -X POST -H "Content-type: application/json" --data '{"text":"'"$INPUT_MESSAGE"'"}' $INPUT_SLACK_WEBHOOK
 
-echo "PAYLOAD = $SLACK_PAYLOAD"
+curl -X POST -H "Content-type: application/json" --data '{"attachments": [{"mrkdwn_in": ["text"],"author_name": "'"$GITHUB_ACTOR"'","author_link": "https://github.com/'"$GITHUB_ACTOR"'","title": "1 new commit pushed to '"$GITHUB_HEAD_REF"'","title_link": "'"$INPUT_PR_LINK"'","text": "`'"${GITHUB_SHA::8}"'"` - '"$INPUT_MESSAGE"'"} ]}' $INPUT_SLACK_WEBHOOK
 
-curl -X POST -H "Content-type: application/json" --data '{"text":"'"$INPUT_MESSAGE"'"}' $INPUT_SLACK_WEBHOOK
+
+# {
+#     "attachments": [
+#         {
+#             "mrkdwn_in": ["text"],
+#             "author_name": "'"$GITHUB_ACTOR"'",
+#             "author_link": "https://github.com/'"$GITHUB_ACTOR"'",
+#             "title": "1 new commit pushed to '"$GITHUB_HEAD_REF"'",
+#             "title_link": "'"$INPUT_PR_LINK"'",
+#             "text": "'"${GITHUB_SHA::8}"'" - '"$INPUT_MESSAGE"'"
+#         }
+#     ]
+# }
